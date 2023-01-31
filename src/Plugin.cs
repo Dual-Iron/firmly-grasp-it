7efd0fa09fb26d6b795b33166c086ada6950a6ef
Dual-Iron <31146412+Dual-Iron@users.noreply.github.com>
2023-01-31 18:57:48 -0500

    Fix shortcut pathing

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -8,6 +8,7 @@ using BepInEx;
 using System.Security.Permissions;
 using RWCustom;
 using static AItile;
+using static MovementConnection;
 
 #pragma warning disable CS0618 // Do not remove the following line.
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
@@ -76,6 +77,8 @@ sealed class Plugin : BaseUnityPlugin
 
         // Also start recalculating paths for every critter in the room.
         RedoPathfinders(room);
+
+        Logger.LogDebug($"Pathing updated for {room.abstractRoom.name}");
     }
 
     private static void SetAccessibility(Room room, IntVector2 pos)
@@ -119,10 +122,22 @@ sealed class Plugin : BaseUnityPlugin
 
     private static void RedoConnections(Room room)
     {
+        static bool IsSetByAimapper(MovementType ty)
+        {
+            // See SetConnections. This includes all movement types that will be recalculated because of a spear's placement.
+            return ty switch {
+                MovementType.Standard or MovementType.ReachUp or MovementType.DoubleReachUp or MovementType.ReachDown or MovementType.DropToFloor or
+                MovementType.DropToClimb or MovementType.DropToWater or MovementType.ReachOverGap or MovementType.LizardTurn or MovementType.Slope or
+                MovementType.OpenDiagonal or MovementType.CeilingSlope or MovementType.SemiDiagonalReach => true,
+                _ => false,
+            };
+        }
+        static bool IsReset(MovementConnection conn) => IsSetByAimapper(conn.type);
+
         for (int y = 0; y < room.TileHeight; y++) {
             for (int x = 0; x < room.TileWidth; x++) {
-                room.aimap.map[x, y].outgoingPaths.Clear();
-                room.aimap.map[x, y].incomingPaths.Clear();
+                room.aimap.map[x, y].outgoingPaths.RemoveAll(IsReset);
+                room.aimap.map[x, y].incomingPaths.RemoveAll(IsReset);
             }
         }
         for (int y = 0; y < room.TileHeight; y++) {
@@ -168,7 +183,7 @@ sealed class Plugin : BaseUnityPlugin
 
         for (int i = 0; i < 4; i++) {
             if (pos.x + Custom.fourDirections[i].x >= 0 && pos.x + Custom.fourDirections[i].x < room.TileWidth && pos.y + Custom.fourDirections[i].y >= 0 && pos.y + Custom.fourDirections[i].y < room.TileHeight) {
-                map.map[x, y].outgoingPaths.Add(new MovementConnection(MovementConnection.MovementType.Standard, WrldCrd(pos), WrldCrd(pos + Custom.fourDirections[i]), 1));
+                map.map[x, y].outgoingPaths.Add(new(MovementType.Standard, WrldCrd(pos), WrldCrd(pos + Custom.fourDirections[i]), 1));
             }
             if (room.GetTile(pos + Custom.eightDirections[i]).Terrain == Room.Tile.TerrainType.Solid && room.GetTile(pos - Custom.eightDirections[i]).Terrain == Room.Tile.TerrainType.Solid) {
                 map.map[x, y].narrowSpace = true;
@@ -178,13 +193,13 @@ sealed class Plugin : BaseUnityPlugin
         IntVector2 up = new(0, 1);
 
         if (map.getAItile(pos + up).acc != Accessibility.Solid && map.getAItile(pos + up * 2).walkable && map.getAItile(pos + up).acc > map.getAItile(pos).acc && map.getAItile(pos + up * 2).acc < map.getAItile(pos + up).acc) {
-            map.map[x, y].outgoingPaths.Add(new MovementConnection(MovementConnection.MovementType.ReachUp, WrldCrd(pos), WrldCrd(pos + up * 2), 2));
+            map.map[x, y].outgoingPaths.Add(new(MovementType.ReachUp, WrldCrd(pos), WrldCrd(pos + up * 2), 2));
         }
         if (map.getAItile(pos + up * 3).walkable && map.getAItile(pos + up).acc != Accessibility.Solid && map.getAItile(pos + up * 2).acc != Accessibility.Solid && map.getAItile(pos + up).acc > map.getAItile(pos).acc && map.getAItile(pos + up).acc > map.getAItile(pos + up * 3).acc && map.getAItile(pos + up * 2).acc > map.getAItile(pos).acc && map.getAItile(pos + up * 2).acc > map.getAItile(pos + up * 3).acc) {
-            map.map[x, y].outgoingPaths.Add(new MovementConnection(MovementConnection.MovementType.DoubleReachUp, WrldCrd(pos), WrldCrd(pos + up * 3), 3));
+            map.map[x, y].outgoingPaths.Add(new(MovementType.DoubleReachUp, WrldCrd(pos), WrldCrd(pos + up * 3), 3));
         }
         if (map.getAItile(pos - up).acc != Accessibility.Solid && map.getAItile(pos - up * 2).walkable && map.getAItile(pos - up).acc > map.getAItile(pos).acc && map.getAItile(pos - up * 2).acc < map.getAItile(pos + up).acc) {
-            map.map[x, y].outgoingPaths.Add(new MovementConnection(MovementConnection.MovementType.ReachDown, WrldCrd(pos), WrldCrd(pos - up * 2), 2));
+            map.map[x, y].outgoingPaths.Add(new(MovementType.ReachDown, WrldCrd(pos), WrldCrd(pos - up * 2), 2));
         }
 
         IntVector2 down = new(0, -1);
@@ -194,18 +209,18 @@ sealed class Plugin : BaseUnityPlugin
             List<int> list = new();
             for (int j = pos.y - 1; j > 0; j--) {
                 if (map.getAItile(pos.x, j).acc == Accessibility.Floor && (room.GetTile(pos.x, j - 1).Terrain == Room.Tile.TerrainType.Solid || room.GetTile(pos.x, j - 1).Terrain == Room.Tile.TerrainType.Floor)) {
-                    map.map[x, y].outgoingPaths.Add(new MovementConnection(MovementConnection.MovementType.DropToFloor, WrldCrd(pos), WrldCrd(new IntVector2(pos.x, j)), pos.y - j));
+                    map.map[x, y].outgoingPaths.Add(new(MovementType.DropToFloor, WrldCrd(pos), WrldCrd(new IntVector2(pos.x, j)), pos.y - j));
                     list.Add(j);
                     list.Add(j - 1);
                     list.Add(j + 1);
                     break;
                 }
                 if (map.getAItile(pos.x, j).acc == Accessibility.Climb && map.getAItile(pos.x, j + 1).acc > map.getAItile(pos.x, j).acc) {
-                    map.map[x, y].outgoingPaths.Add(new MovementConnection(MovementConnection.MovementType.DropToClimb, WrldCrd(pos), WrldCrd(new IntVector2(pos.x, j)), pos.y - j));
+                    map.map[x, y].outgoingPaths.Add(new(MovementType.DropToClimb, WrldCrd(pos), WrldCrd(new IntVector2(pos.x, j)), pos.y - j));
                     list.Add(j);
                 }
                 if (room.GetTile(pos.x, j).WaterSurface) {
-                    map.map[x, y].outgoingPaths.Add(new MovementConnection(MovementConnection.MovementType.DropToWater, WrldCrd(pos), WrldCrd(new IntVector2(pos.x, j)), pos.y - j));
+                    map.map[x, y].outgoingPaths.Add(new(MovementType.DropToWater, WrldCrd(pos), WrldCrd(new IntVector2(pos.x, j)), pos.y - j));
                     list.Add(j);
                 }
                 num2++;
@@ -216,14 +231,14 @@ sealed class Plugin : BaseUnityPlugin
                     while (num3 > 0 && map.getAItile(pos.x + k, num3).acc != Accessibility.Solid && map.getAItile(pos.x, num3).acc != Accessibility.Solid) {
                         if (map.getAItile(pos.x + k, num3).acc == Accessibility.Floor && map.getAItile(pos.x + k, num3 + 1).acc != Accessibility.Solid && map.getAItile(pos.x + k, num3 + 2).acc != Accessibility.Solid && (room.GetTile(pos.x + k, num3 - 1).Terrain == Room.Tile.TerrainType.Solid || room.GetTile(pos.x + k, num3 - 1).Terrain == Room.Tile.TerrainType.Floor) && !IsThereAStraightDropBetweenTiles(new IntVector2(pos.x - 1, pos.y), new IntVector2(pos.x + k, num3)) && !IsThereAStraightDropBetweenTiles(new IntVector2(pos.x + 1, pos.y), new IntVector2(pos.x + k, num3))) {
                             if (!list.Contains(num3)) {
-                                map.map[x, y].outgoingPaths.Add(new MovementConnection(MovementConnection.MovementType.DropToFloor, WrldCrd(pos), WrldCrd(new IntVector2(pos.x + k, num3)), pos.y - num3));
+                                map.map[x, y].outgoingPaths.Add(new(MovementType.DropToFloor, WrldCrd(pos), WrldCrd(new IntVector2(pos.x + k, num3)), pos.y - num3));
                                 break;
                             }
                             break;
                         }
                         else {
                             if (map.getAItile(pos.x + k, num3).acc == Accessibility.Climb && map.getAItile(pos.x + k, num3 + 1).acc != Accessibility.Solid && map.getAItile(pos.x + k, num3 + 2).acc != Accessibility.Solid && map.getAItile(pos.x + k, num3 + 1).acc > map.getAItile(pos.x + k, num3).acc && !IsThereAStraightDropBetweenTiles(new IntVector2(pos.x - 1, pos.y), new IntVector2(pos.x + k, num3)) && !IsThereAStraightDropBetweenTiles(new IntVector2(pos.x + 1, pos.y), new IntVector2(pos.x + k, num3)) && !list.Contains(num3)) {
-                                map.map[x, y].outgoingPaths.Add(new MovementConnection(MovementConnection.MovementType.DropToClimb, WrldCrd(pos), WrldCrd(new IntVector2(pos.x + k, num3)), pos.y - num3));
+                                map.map[x, y].outgoingPaths.Add(new(MovementType.DropToClimb, WrldCrd(pos), WrldCrd(new IntVector2(pos.x + k, num3)), pos.y - num3));
                             }
                             num3--;
                         }
@@ -235,35 +250,35 @@ sealed class Plugin : BaseUnityPlugin
             IntVector2 h = new(l == 0 ? -1 : 1, 0);
 
             if (map.getAItile(pos + h).acc != Accessibility.Solid && map.getAItile(pos + h * 2).walkable && map.getAItile(pos + h).acc > map.getAItile(pos).acc && map.getAItile(pos + h * 2).acc < map.getAItile(pos + h).acc) {
-                map.map[x, y].outgoingPaths.Add(new MovementConnection(MovementConnection.MovementType.ReachOverGap, WrldCrd(pos), WrldCrd(pos + h * 2), 2));
+                map.map[x, y].outgoingPaths.Add(new(MovementType.ReachOverGap, WrldCrd(pos), WrldCrd(pos + h * 2), 2));
             }
             if (map.getAItile(pos).acc == Accessibility.Floor && map.getAItile(pos + h).acc == Accessibility.Floor && map.getAItile(pos + new IntVector2(0, 1)).acc != Accessibility.Solid && map.getAItile(pos + h + new IntVector2(0, 1)).acc != Accessibility.Solid) {
-                map.map[x, y].outgoingPaths.Add(new MovementConnection(MovementConnection.MovementType.LizardTurn, WrldCrd(pos), WrldCrd(pos + h), 1));
+                map.map[x, y].outgoingPaths.Add(new(MovementType.LizardTurn, WrldCrd(pos), WrldCrd(pos + h), 1));
             }
             if (map.getAItile(pos).acc == Accessibility.Floor && room.GetTile(pos + h).Terrain == Room.Tile.TerrainType.Slope && map.getAItile(pos + new IntVector2(h.x, 1)).acc == Accessibility.Floor) {
-                map.map[x, y].outgoingPaths.Add(new MovementConnection(MovementConnection.MovementType.Slope, WrldCrd(pos), WrldCrd(pos + new IntVector2(h.x, 1)), 1));
-                map.map[x + h.x, y + 1].outgoingPaths.Add(new MovementConnection(MovementConnection.MovementType.Slope, WrldCrd(pos + new IntVector2(h.x, 1)), WrldCrd(pos), 1));
+                map.map[x, y].outgoingPaths.Add(new(MovementType.Slope, WrldCrd(pos), WrldCrd(pos + new IntVector2(h.x, 1)), 1));
+                map.map[x + h.x, y + 1].outgoingPaths.Add(new(MovementType.Slope, WrldCrd(pos + new IntVector2(h.x, 1)), WrldCrd(pos), 1));
             }
             else if (map.getAItile(pos + new IntVector2(h.x, 0)).acc != Accessibility.Solid && map.getAItile(pos + new IntVector2(0, 1)).acc != Accessibility.Solid) {
                 int num4 = Math.Max((int)map.getAItile(pos).acc, (int)map.getAItile(pos + new IntVector2(h.x, 1)).acc);
                 if (map.getAItile(pos + new IntVector2(h.x, 0)).acc > (Accessibility)num4 && map.getAItile(pos + new IntVector2(0, 1)).acc > (Accessibility)num4) {
-                    map.map[x, y].outgoingPaths.Add(new MovementConnection(MovementConnection.MovementType.OpenDiagonal, WrldCrd(pos), WrldCrd(pos + new IntVector2(h.x, 1)), 1));
-                    map.map[x + h.x, y + 1].outgoingPaths.Add(new MovementConnection(MovementConnection.MovementType.OpenDiagonal, WrldCrd(pos + new IntVector2(h.x, 1)), WrldCrd(pos), 1));
+                    map.map[x, y].outgoingPaths.Add(new(MovementType.OpenDiagonal, WrldCrd(pos), WrldCrd(pos + new IntVector2(h.x, 1)), 1));
+                    map.map[x + h.x, y + 1].outgoingPaths.Add(new(MovementType.OpenDiagonal, WrldCrd(pos + new IntVector2(h.x, 1)), WrldCrd(pos), 1));
                 }
             }
             if ((map.getAItile(pos).acc == Accessibility.Ceiling || map.getAItile(pos).acc == Accessibility.Wall) && room.GetTile(pos + h).Terrain == Room.Tile.TerrainType.Slope && (map.getAItile(pos + new IntVector2(h.x, -1)).acc == Accessibility.Ceiling || map.getAItile(pos + new IntVector2(h.x, -1)).acc == Accessibility.Wall)) {
-                map.map[x, y].outgoingPaths.Add(new MovementConnection(MovementConnection.MovementType.CeilingSlope, WrldCrd(pos), WrldCrd(pos + new IntVector2(h.x, -1)), 1));
-                map.map[x + h.x, y - 1].outgoingPaths.Add(new MovementConnection(MovementConnection.MovementType.CeilingSlope, WrldCrd(pos + new IntVector2(h.x, -1)), WrldCrd(pos), 1));
+                map.map[x, y].outgoingPaths.Add(new(MovementType.CeilingSlope, WrldCrd(pos), WrldCrd(pos + new IntVector2(h.x, -1)), 1));
+                map.map[x + h.x, y - 1].outgoingPaths.Add(new(MovementType.CeilingSlope, WrldCrd(pos + new IntVector2(h.x, -1)), WrldCrd(pos), 1));
                 map.map[x, y].outgoingPaths.Add(map.map[x + h.x, y - 1].outgoingPaths[map.map[x + h.x, y - 1].outgoingPaths.Count - 1]);
             }
             if (map.getAItile(pos + new IntVector2(h.x, -2)).walkable && map.getAItile(pos + new IntVector2(0, -1)).acc != Accessibility.Solid && map.getAItile(pos + new IntVector2(h.x, -1)).acc != Accessibility.Solid && (map.getAItile(pos + new IntVector2(h.x, 0)).acc != Accessibility.Solid || map.getAItile(pos + new IntVector2(0, -2)).acc != Accessibility.Solid) && map.getAItile(pos + new IntVector2(h.x, 0)).acc > map.getAItile(pos).acc && map.getAItile(pos + new IntVector2(h.x, 0)).acc > map.getAItile(pos + new IntVector2(h.x, -2)).acc && map.getAItile(pos + new IntVector2(0, -2)).acc > map.getAItile(pos).acc && map.getAItile(pos + new IntVector2(0, -2)).acc > map.getAItile(pos + new IntVector2(h.x, -2)).acc && map.getAItile(pos + new IntVector2(0, -1)).acc > map.getAItile(pos).acc && map.getAItile(pos + new IntVector2(0, -1)).acc > map.getAItile(pos + new IntVector2(h.x, -2)).acc && map.getAItile(pos + new IntVector2(h.x, -1)).acc > map.getAItile(pos).acc && map.getAItile(pos + new IntVector2(h.x, -1)).acc > map.getAItile(pos + new IntVector2(h.x, -2)).acc) {
-                map.map[x, y].outgoingPaths.Add(new MovementConnection(MovementConnection.MovementType.SemiDiagonalReach, WrldCrd(pos), WrldCrd(pos + new IntVector2(h.x, -2)), 3));
-                map.map[x + h.x, y - 2].outgoingPaths.Add(new MovementConnection(MovementConnection.MovementType.SemiDiagonalReach, WrldCrd(pos + new IntVector2(h.x, -2)), WrldCrd(pos), 3));
+                map.map[x, y].outgoingPaths.Add(new(MovementType.SemiDiagonalReach, WrldCrd(pos), WrldCrd(pos + new IntVector2(h.x, -2)), 3));
+                map.map[x + h.x, y - 2].outgoingPaths.Add(new(MovementType.SemiDiagonalReach, WrldCrd(pos + new IntVector2(h.x, -2)), WrldCrd(pos), 3));
                 map.map[x, y].outgoingPaths.Add(map.map[x + h.x, y - 2].outgoingPaths[map.map[x + h.x, y - 2].outgoingPaths.Count - 1]);
             }
             if (map.getAItile(pos + new IntVector2(-2, h.x)).walkable && map.getAItile(pos + new IntVector2(-1, 0)).acc != Accessibility.Solid && map.getAItile(pos + new IntVector2(-1, h.x)).acc != Accessibility.Solid && (map.getAItile(pos + new IntVector2(0, h.x)).acc != Accessibility.Solid || map.getAItile(pos + new IntVector2(-2, 0)).acc != Accessibility.Solid) && map.getAItile(pos + new IntVector2(0, h.x)).acc > map.getAItile(pos).acc && map.getAItile(pos + new IntVector2(0, h.x)).acc > map.getAItile(pos + new IntVector2(-2, h.x)).acc && map.getAItile(pos + new IntVector2(-2, 0)).acc > map.getAItile(pos).acc && map.getAItile(pos + new IntVector2(-2, 0)).acc > map.getAItile(pos + new IntVector2(-2, h.x)).acc && map.getAItile(pos + new IntVector2(-1, 0)).acc > map.getAItile(pos).acc && map.getAItile(pos + new IntVector2(-1, 0)).acc > map.getAItile(pos + new IntVector2(-2, h.x)).acc && map.getAItile(pos + new IntVector2(-1, h.x)).acc > map.getAItile(pos).acc && map.getAItile(pos + new IntVector2(-1, h.x)).acc > map.getAItile(pos + new IntVector2(-2, h.x)).acc) {
-                map.map[x, y].outgoingPaths.Add(new MovementConnection(MovementConnection.MovementType.SemiDiagonalReach, WrldCrd(pos), WrldCrd(pos + new IntVector2(-2, h.x)), 3));
-                map.map[x - 2, y + h.x].outgoingPaths.Add(new MovementConnection(MovementConnection.MovementType.SemiDiagonalReach, WrldCrd(pos + new IntVector2(-2, h.x)), WrldCrd(pos), 3));
+                map.map[x, y].outgoingPaths.Add(new(MovementType.SemiDiagonalReach, WrldCrd(pos), WrldCrd(pos + new IntVector2(-2, h.x)), 3));
+                map.map[x - 2, y + h.x].outgoingPaths.Add(new(MovementType.SemiDiagonalReach, WrldCrd(pos + new IntVector2(-2, h.x)), WrldCrd(pos), 3));
                 map.map[x, y].outgoingPaths.Add(map.map[x - 2, y + h.x].outgoingPaths[map.map[x - 2, y + h.x].outgoingPaths.Count - 1]);
             }
         }
